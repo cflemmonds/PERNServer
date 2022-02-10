3f9819ff6a0a06ff10f6c2f092b4de28781af239
allenbra17 <allenbra17@gmail.com>
2022-02-10 16:33:13 -0500

    moved logo to client side

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(Express.json())
 
 const controllers = require("./controllers");
 
-app.use("/property", controllers.propController)
+app.use("/property", controllers.propertyController)
 
 dbConnection.authenticate()
     .then(() => dbConnection.sync())
