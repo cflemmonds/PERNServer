cc580bc137ddf5adbfb5547fc70f1dfae2512727
Chris Flemmonds <cflemmonds@gmail.com>
2022-02-10 11:40:49 -0500

    user register endpoint tested, test user created, adding bcrypt and jwt next

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const Express = require('express')
 const app = Express()
 const dbConnection = require("./db")
 
-// app.use('/test', (req, res) => {
-//     res.send('This is message from the test endpoint on the server!')
-// })
+app.use(Express.json())
 
 const controllers = require("./controllers");
 
 app.use("/prop", controllers.propController)
 app.use("/user", controllers.userController)
-
-app.listen(4000, () => {
-    console.log(`[Server]: App is listening on 4000.`)
-})
 
 dbConnection.authenticate()
     .then(() => dbConnection.sync())
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,50 +1,22 @@
-const router = require('express').Router
-const {UserModel} = require("../models")
+const router = require('express').Router();
+const { UserModel } = require("../models")
 
 router.post("/register", async (req, res) => {
-    const { firstName, lastName, email, password } = req.body
 
-    try {
-        const newUser = await UserModel.create({
-            firstName,
-            lastName,
-            email,
-            password: bcrypt.hashSync(password, 10),
-        })
-        const token = jwt.sign({
-            id: newUser.id},
-            process.env.JWT_SECRET_KEY,
-            {expiresIn: 60 * 60 * 24}
-        )
-            
+    const { firstName, lastName, email, password } = req.body;
 
-        res.status(201).json({
-            message: "User created",
-            user: newUser,
-            token
-        })
-    } catch (err) {
-        // console.log(err)
-        // res.status(500).json({
-        // error: err
-    // })
-        if (err.name === "SequelizeUniqueConstraintError"){
-            res.status(409).json({
-                message: "Email already in use."
-            })
-        } else {
-            res.status(500).json({
-                message: "You done messed up and I don't know where."
-            })
-        }
-
-    }
+    await UserModel.create({
+        firstName: "test",
+        lastName: "testerson",
+        email: "test@test.com",
+        password: "testingme"
+    });
+    res.send("This is our user/register endpoint!")
 })
 
-router.post("/login")
 
 
 
 
 
-module.exports= router;
+module.exports = router;
