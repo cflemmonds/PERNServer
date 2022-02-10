31875a7d1b6edc9ddf9c84a436e37c3e2c2e7e2f
allenbra17 <allenbra17@gmail.com>
2022-02-10 11:41:00 -0500

    recreated controller files

---
+++ controllers/usercontroller.js
@@ -0,0 +1,12 @@
+const Express = require('express');
+const router = Express.Router();
+
+router.get('/practice', (req, res) => {
+    res.send('Hey, have a drink!')
+});
+
+router.post("/user")
+
+router.post("/user/create")
+
+module.exports = router;
